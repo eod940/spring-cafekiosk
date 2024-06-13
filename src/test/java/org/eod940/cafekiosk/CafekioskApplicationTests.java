0f9e3624c767e87ece7a38f232fe93f8d9af4f9d
eod940 <eod940@gmail.com>
2024-06-13 22:43:49 +0900

    init: 🎉 project init

---
+++ src/test/java/org/eod940/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eod940.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
